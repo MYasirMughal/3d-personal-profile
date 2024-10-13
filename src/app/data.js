3df0caf12e7119923ac104113c5abe0b334c7100
MYasirMughal <144555237+MYasirMughal@users.noreply.github.com>
2024-10-13 19:42:43 +0500

    Update data.js

--- src/app/data.js
+++ src/app/data.js
@@ -1,15 +1,15 @@
 export const projectsData = [
   {
     id: 1,
-    name: "Ajman City Client Project",
+    name: "Ajman City Client Project",<br> "Coming Soon",
     description: "Client project for Ajman City, UAE",
-    date: "2024-10-09",
+    //date: "2024-10-09",
     demoLink: "#",
   },
   {
     id: 2,
     name: "Coral Shopping Store",
-    description: "E-commerce platform for shopping", <br> "Coming Soon",
+    description: "E-commerce platform for shopping",
    // date: "Coming Soon",
     demoLink: "#",
   },
